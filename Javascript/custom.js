7d53bcaee35e698d5c4daaec888a76866d782b16
reedgannon <reedgannon@gmail.com>
2020-05-22 15:07:45 -0400

    final java

--- Javascript/custom.js
+++ Javascript/custom.js
@@ -17,4 +17,45 @@ $(document).ready(function(){
       })
     }
   });
+
+});
+
+
+
+mapboxgl.accessToken = 'pk.eyJ1IjoicmVlZGdhbm5vbiIsImEiOiJja2FpZGEyc3YwYngyMndwYzN4ejFqNGMyIn0.ZeU0LataXoFh5s2DkpyzsA';
+var map = new mapboxgl.Map({
+    container: 'map',
+    center: [40.794, -74.470],
+    zoom: 12.89,
+    style: 'mapbox://styles/jmezavin/ck4063g7e121s1cprqardiu2j'
+    });
+
+var markerLoui = new mapboxgl.Marker();
+markerLoui.setLngLat([40.799,-74.481]);
+markerLoui.addTo(map);
+
+var markerBurger = new mapboxgl.Marker();
+markerBurger.setLngLat([40.794,-74.478]);
+markerBurger.addTo(map);
+
+var markerClash = new mapboxgl.Marker();
+markerClash.setLngLat([40.794,-74.478]);
+markerClash.addTo(map);
+
+var markerJunction = new mapboxgl.Marker();
+markerJunction.setLngLat([40.792,-74.482]);
+markerJunction.addTo(map);
+
+var markerTor = new mapboxgl.Marker();
+markerTor.setLngLat([40.797,-74.467]);
+markerTor.addTo(map);
+
+var markerReich = new mapboxgl.Marker();
+markerReich.setLngLat([40.805,-74.474]);
+markerReich.addTo(map);
+
+map.on('click',function(e){
+    console.log("the longitude of where the user's clicking is at " + e.lngLat.lng);
+    console.log("the latitude of where the user's clicking is at " + e.lngLat.lat);
+    console.log(e);
 });
